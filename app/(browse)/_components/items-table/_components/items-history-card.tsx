c725e8d15f33e9a2877e0d74472b3616111d71b6
kanandvardhan <kanandvardhan@gmail.com>
2023-12-28 01:50:33 +0530

    Added Additional History feature
    History feature enables user to add same item to inventory instantly

---
+++ app/(browse)/_components/items-table/_components/items-history-card.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { addItems } from "./add-items-form";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { RefreshCw } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface ItemsHistoryCardProps {
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+}
+
+export const ItemsHistoryCard = ({
+  description,
+  name,
+  price,
+  quantity,
+}: ItemsHistoryCardProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const postItem = async () => {
+    const values = { description, name, price, quantity };
+    const result = await addItems(values);
+
+    if (result.status === "success") {
+      if (typeof window !== "undefined") {
+        await JSON.parse(localStorage.getItem("grocery-items") || "[]");
+      }
+
+      toast.success(result.message);
+      redirect("/");
+    } else {
+      toast.error(result.message);
+    }
+  };
+
+  return (
+    <div
+      onClick={() => startTransition(() => postItem())}
+      className="group flex flex-col cursor-pointer relative p-5 rounded-xl"
+    >
+      <div className="group-hover:opacity-90 group-hover:backdrop-blur-2xl transition absolute top-0 left-0 h-full w-full bg-stone-800/40  opacity-0 rounded-xl">
+        <div className="flex items-center justify-center h-full ">
+          {isPending ? (
+            <RefreshCw className={cn("animate-spin")} />
+          ) : (
+            <p className="text-xl font-bold">Add again</p>
+          )}
+        </div>
+      </div>
+      <p>Name: {name}</p>
+      <p>Description: {description}</p>
+      <p>Price: {price}</p>
+      <p>Quantity: {quantity}</p>
+    </div>
+  );
+};
